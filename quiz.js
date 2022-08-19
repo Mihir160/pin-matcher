982cd6e592d49227662121187f5eee94c7579bb8
Mihir160 <mihir.roy266@gmail.com>
2022-08-19 00:36:55 -0700

    second

---
+++ quiz.js
@@ -0,0 +1,26 @@
+// let count = 0;
+// console.log(parseInt("count"+ 1));
+
+// console.log(Math.pow(4, 0.5)+"spiderman"+"987"+Math.sqrt(9));
+
+// function generatePin(){ 
+//     return Math.floor(Math.random()*90000) + 10000; 
+// }
+// const generatePin1 = generatePin();
+// console.log(generatePin1);
+
+
+
+// if(!isNaN('10X')){
+//   return;
+// }
+
+// const product = 5; const price = "7"; 
+// const subTotal = product * price; 
+// const tax = subTotal/10; 
+// const total = subTotal + tax;
+// console.log(total);
+
+const category="pen"; 
+const performer = category+"-teller";
+console.log(performer);
